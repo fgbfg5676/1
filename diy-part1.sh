fdfc8df9f96b615adda7b8d35719fcb3b31eeec2
fgbfg5676 <niwo5507@gmail.com>
2025-08-06 16:49:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,7 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default  # 常用插件集合
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default  # 小型插件补充
 
